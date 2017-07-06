7341a0ca219ae095d63c5209a45a6e753959aaa7
Louis Warren <louis@lsw.nz>
2017-07-07 11:14:51 +1200

    add cut plotting

--- plot.py
+++ plot.py
@@ -23,8 +23,9 @@ def plotnext(px, py, plevel, cuts=(), max_level=5, removed=False):
     if plevel > max_level:
         return
     sx = 1/(2**plevel)
-    sy = 1/plevel
-    qy = py - sy
+    qy = py - 1
+    if plevel < 10:
+        print('\t' * plevel + 'Level' + str(plevel) + '...')
     for val, shift in (('0', -sx), ('1', sx)):
         qx = px + shift
         if not removed and val not in cuts:
@@ -33,9 +34,31 @@ def plotnext(px, py, plevel, cuts=(), max_level=5, removed=False):
         else:
             ax.plot((px, qx), (py, qy), color='red', **plotopts)
             plotnext(qx, qy, plevel + 1, trimcuts(val, cuts), max_level, True)
+    if plevel < 10:
+        print('\t' * plevel + 'Level' + str(plevel) + 'COMPLETE')
+
+def plotcuts(px, py, plevel, cuts=(), max_level=5):
+    if plevel > max_level:
+        return
+    sx = 1/(2**plevel)
+    qy = py - 1
+    r = False
+    if plevel < 16:
+        print('\t' * plevel + 'Level', str(plevel))
+    for val, shift in (('0', -sx), ('1', sx)):
+        qx = px + shift
+        if val not in cuts:
+            c = plotcuts(qx, qy, plevel + 1, trimcuts(val, cuts), max_level)
+            if c:
+                ax.plot((px, qx), (py, qy), color='black', **plotopts)
+                r = True
+        else:
+            ax.plot((px, qx), (py, qy), color='red', **plotopts)
+            return True
+    return r
 
 
-
-plotnext(0, 0, 1, cuts, 16)
+#plotnext(0, 0, 1, cuts, 10)
+plotcuts(0, 0, 1, cuts, 26)
 plt.show()
 
