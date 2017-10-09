5e0d5d3b360f1e28d642297aa2fc4127dfde0210
Louis Warren <louis@lsw.nz>
2017-10-09 16:10:13 +0100

    Finish kleene rewrite

--- kleene.py
+++ kleene.py
@@ -1,99 +1,146 @@
+# Function composition
+
 from collections import defaultdict
 
-from tm import *
+compose = lambda f: lambda g: lambda *a, **k: f(g(*a, **k))
 
-def halting_tm(instructions, max_runtime=100):
-    tape_history = set()
-    tape, p, t = Tape(), 0, 0
-    jumps = jump_table(instructions)
-    prognum = int('0' + instructions, 6)
-    while p < len(instructions):
-        x = instructions[p]
-        if   x == I.OutputVal: yield tape[t]
-        elif x == I.ToggleVal: tape[t] = 1 - tape[t]
-        elif x == I.MoveFward: t += 1
-        elif x == I.MoveBward: t = t - 1 if t else 0
-        elif x == I.JmpFIfNot: p = jumps[p] if tape[t] == 0 else p
-        elif x == I.JmpBIfSet: p = jumps[p] if tape[t] == 1 else p
-        else: raise NotImplementedError(x)
-        if x == I.OutputVal:
-            tape_history = set()
+class I:
+    '''Instruction set for turing machine.'''
+    OutputVal = '0'            # Output current cell
+    ToggleVal = '1'            # Toggle current cell
+    MoveFward = '2'            # Move tape pointer forward
+    MoveBward = '3'            # Move tape pointer backward
+    JmpFIfNot = '4'            # Jump forward to matching '5' if cell is 0
+    JmpBIfOne = '5'            # Jump backward to matching '4' if cell is 1
+    # Optimally, each JmpFIfNot would match a JmpBIfOne (allowing for nesting).
+    # On a mismatch, the jump target is the beginning of the program instead.
+
+
+class Tape(list):
+    '''Automatically expanding tape.'''
+    def _expand(self, n):
+        self += [0] * (n - len(self))
+
+    def __getitem__(self, i):
+        self._expand(i + 1)
+        return super().__getitem__(i)
+
+    def __setitem__(self, i, x):
+        self._expand(i + 1)
+        return super().__setitem__(i, x)
+
+
+@compose(dict)
+def build_jump_table(instrs):
+    '''Precompute jump targets.'''
+    jump_stack = []
+    for ctr, instr in enumerate(instrs):
+        if instr == I.JmpFIfNot:
+            jump_stack.append(ctr)
+        elif instr == I.JmpBIfOne:
+            if jump_stack:
+                s = jump_stack.pop()
+                yield s, ctr
+                yield ctr, s
+            else:
+                yield ctr, 0
+    for ctr in jump_stack:
+        yield ctr, 0
+
+
+def transition(instr, jumps, tape, ptr, ctr):
+    '''Perform a single state turing machine state transition.'''
+    output = None
+    if   instr == I.OutputVal: output    = tape[ptr]
+    elif instr == I.ToggleVal: tape[ptr] = 1 - tape[ptr]
+    elif instr == I.MoveFward: ptr       = ptr + 1
+    elif instr == I.MoveBward: ptr       = ptr - 1 if ptr else 0
+    elif instr == I.JmpFIfNot: ctr       = jumps[ctr] if tape[ptr] == 0 else ctr
+    elif instr == I.JmpBIfOne: ctr       = jumps[ctr] if tape[ptr] == 1 else ctr
+    else: raise NotImplementedError(repr(instr))
+    ctr += 1
+    return output, tape, ptr, ctr
+
+
+def htm(instrs, max_runfactor=100):
+    '''Co-routine for an automatically halting machine.
+
+    Not a real turing machine, as it halts after a too-long runtime or infinite
+    non-outputting loop.'''
+    state_history = set()
+    max_runtime = len(instrs) * max_runfactor
+    tape, ptr, ctr = Tape(), 0, 0
+    jumps = build_jump_table(instrs)
+    while ctr < len(instrs):
+        output, tape, ptr, ctr = transition(instrs[ctr], jumps, tape, ptr, ctr)
+        yield output
+        if output is not None:
+            state_history = set()
+        state = (tuple(tape), ptr, ctr)
+        if state in state_history or len(state_history) > max_runtime:
+            return
         else:
-            yield
-        p += 1
-        if (p, t, str(tape)) in tape_history or \
-                len(tape_history) > prognum * max_runtime or \
-                len(tape) > max_runtime:
-            break
-        else:
-            tape_history.add((p, t, str(tape)))
+            state_history.add(state)
 
+def get_prog(index):
+    '''Get the program for a given index.'''
+    prog = ''
+    while index:
+        prog = str(index % 6) + prog
+        index //= 6
+    return prog
 
-def simulate():
-    machines, outputs = {}, defaultdict(lambda: '')
-    try:
-        n = 0
-        while True:
-            machines[n] = tm(int_to_base6(n))
-            completed = []
-            for m in list(machines.keys()):
-                try:
-                    v = next(machines[m])
-                    if v is not None:
-                        outputs[m] += str(v)
-                except StopIteration:
-                    yield m, outputs[m]
-                    del machines[m]
-                    if m in outputs:
-                        del outputs[m]
-            n += 1
-    except KeyboardInterrupt:
-        pass
-    finally:
-        for m in machines.keys():
-            yield m, outputs[m] + '-'
+def diagonal_run():
+    '''Run all machines (diagonalised) giving their outputs.
 
-
-import sys
-f = open('log.txt', 'w')
-
-def kleene():
-    machines, outputs = {}, defaultdict(lambda: '')
-    cuts = set()
-    cut_depth = 2
+    Stops displaying outputs of halted machines.'''
     n = 0
-    fmt = "Iter number: {:<16} Number of machines: {:<16}"
+    machines = {}
+    outputs = defaultdict(lambda: '')
     while True:
-        machines[n] = halting_tm(int_to_base6(n))
-        completed = []
-        for m in list(machines.keys()):
-            if any(outputs[m].startswith(cut) for cut in cuts):
-                del machines[m]
-                if m in outputs:
-                    del outputs[m]
-                continue
-            try:
-                v = next(machines[m])
-                if v is not None:
-                    outputs[m] += str(v)
-                if len(outputs[m]) >= cut_depth:
-                    cut = outputs[m][:cut_depth]
-                    cuts.add(cut)
-                    yield cut, m, n
-                    del machines[m]
-                    del outputs[m]
-                    cut_depth += 1
-            except StopIteration:
-                del machines[m]
-                if m in outputs:
-                    del outputs[m]
+        for i in reversed(range(n)):
+            if i in machines:
+                try:
+                    output = next(machines[i])
+                    if output is not None:
+                        outputs[i] += str(output)
+                except StopIteration:
+                    del machines[i]
+                if i in outputs:
+                    print('{:>8}: {}'.format(i, outputs[i]))
+        machines[n] = htm(get_prog(n))
         n += 1
-        if not n % 100:
-            print(fmt.format(n, len(machines)), file=sys.stderr)
-            print(fmt.format(n, len(machines)), file=f)
-            f.flush()
+        print()
 
-if __name__ == '__main__':
-    for cut in kleene():
-        print("Cut {:<16} from machine {:<32} after {:<16} iterations".format(*cut))
-        sys.stdout.flush()
+def kleene_cuts():
+    n = 0
+    cut_depth = 1
+    cuts = {}
+    machines = {}
+    outputs = defaultdict(lambda: '')
+    while True:
+        for i in list(machines.keys()):
+            try:
+                output = next(machines[i])
+                if output is not None:
+                    outputs[i] += str(output)
+                    # Check if this machine has reached a cut
+                    if outputs[i] == cuts.get(len(outputs[i])):
+                        del machines[i]
+                if len(outputs[i]) == cut_depth + 1:
+                    cut_depth += 1
+                    cuts[cut_depth] = outputs[i]
+                    yield cuts[cut_depth]
+                    # Cut all running machines that start with this cut
+                    for j in list(machines):
+                        if outputs[j].startswith(cuts[cut_depth]):
+                            del machines[j]
+            except StopIteration:
+                del machines[i]
+#        print(len(machines))
+        machines[n] = htm(get_prog(n))
+        n += 1
+
+for k in kleene_cuts():
+    print(k)
+
