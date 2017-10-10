6213499906befc01144a2145170f3d71a9251b6c
Louis Warren <louis@lsw.nz>
2017-10-10 12:51:32 +0100

    Actually show cuts in simplified plots

--- plotsimplified.py
+++ plotsimplified.py
@@ -3,18 +3,19 @@ from matplotlib.animation import FuncAnimation
 
 from kleene import kleene_cuts as kleene
 
-def plot_line(px, py, qx, qy, color):
-    ax.plot((px, qx), (py, qy), color=color, linewidth=(-1/qy)**0.5)
+def plot_line(px, py, qx, qy, color='black', linestyle='solid'):
+    ax.plot((px, qx), (py, qy), color=color,
+            linewidth=(-1/qy)**0.5, linestyle=linestyle)
 
 plotted = set()
-def plot_path(cut):
+def plot_cut_args(cut, subpath=False):
     if cut in plotted:
         return
     plotted.add(cut)
     py = (1 - len(cut)) / len(cut) ** 0.5
     px = 0
     for i in range(1, len(cut)):
-        yield from plot_path(cut[:i])
+        yield from plot_cut_args(cut[:i], subpath=True)
         if cut[i-1] == '0':
             px -= 1 / (2 ** i)
         else:
@@ -22,22 +23,28 @@ def plot_path(cut):
     qy = - len(cut) / (len(cut) + 1) ** 0.5
     if cut[-1] == '0':
         qx = px - 1 / (2 ** len(cut))
-        color = 'blue'
+        if subpath:
+            color = 'blue'
+            linestyle = 'solid'
+        else:
+            color = 'darkblue'
+            linestyle = 'dotted'
     else:
         qx = px + 1 / (2 ** len(cut))
-        color = 'red'
-    yield px, py, qx, qy, color
+        if subpath:
+            color = 'red'
+            linestyle = 'solid'
+        else:
+            color = 'brown'
+            linestyle = 'dotted'
+    yield px, py, qx, qy, color, linestyle
 
 
 tree = kleene()
 
 def update(framenum):
     cut = next(tree)
-    for args in plot_path(cut[:-1]):
-        plot_line(*args)
-
-def update_cached(framenum):
-    for args in plot_path(cached_cuts[framenum][:-1]):
+    for args in plot_cut_args(cut):
         plot_line(*args)
 
 fig = plt.figure()
